428da5bcde777f603396a0f15bb7d3d0fc56cc33
Arifin-only <arifin180104@gmail.com>
2024-10-11 00:29:45 +0800

    Create Konversi_Nullable_ke_Non_Nullable.dart

---
+++ Konversi_Nullable_ke_Non_Nullable.dart
@@ -0,0 +1,10 @@
+void main(){
+    String name = 'Arifin';
+    String? nullableName = name;
+    
+    int? nullableNumber;
+    if (nullableNumber != null) {
+        int number = nullableNumber;    
+    }
+    
+}
