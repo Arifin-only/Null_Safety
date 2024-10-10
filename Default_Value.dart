6ffd5ac051082a74233b98efd7abf4d0ee5eb4b7
Arifin-only <arifin180104@gmail.com>
2024-10-11 00:48:48 +0800

    Create Default_Value.dart

---
+++ Default_Value.dart
@@ -0,0 +1,15 @@
+void main(){
+    String? tamu;
+ // tamu = 'Arifin';
+    String namaTamu = tamu ?? 'M.Arifin';
+   
+  /*
+  if(tamu != null){
+       namaTamu = tamu;
+   }else{
+       namaTamu = 'Arifin';
+   }
+  */
+   
+   print(namaTamu);
+}
